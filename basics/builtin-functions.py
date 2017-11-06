93fe8af36cde335c11469514aff738791012f4b5
Adriaan Janse van Rensburg <>
2017-11-06 14:34:45 +1100

    List of built in Python functions

---
+++ basics/builtin-functions.py
@@ -0,0 +1,14 @@
+"""
+Built in Functions
+"""
+print("Hi") # Prints to console
+str(5)  # Converts to string
+int("5")    # Converts to interger
+float("5.6")    # Converts to float number
+bool("True")    # Converts to bool
+len("Hello")    # Returns 5
+len(['hello', 'world'])  # Returns 2
+sorted(['5', '15', '3', '65', '1', '2'])    # Sort from lowest to highest
+sorted(["B", "Z", "a", "M"])    # Sorts alphabetically, Numbers -> Caps -> Lowercase
+user_input = input("Enter a value") # Gets the users inputted value
+# More to come
