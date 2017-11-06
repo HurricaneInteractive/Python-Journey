579bdb1cc1d2d64d48fe771663585c4ec7ec5518
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-06 18:36:01 +1100

    Continue and Break explination

--- basics/conditionals_loops.py
+++ basics/conditionals_loops.py
@@ -15,6 +15,7 @@ elif check == 'Yo':
 else:
     print("It is True")
 
+
 """
 For Loop
 """
@@ -23,6 +24,7 @@ numbers = ["Nick", "Someone", "Bob"]
 # Loops through list and prints out name (item)
 for item in numbers:
     print("Hello", item)
+
 
 """
 While Loops
@@ -37,3 +39,27 @@ while run:
     else:
         print(current)
         current += 1
+
+
+"""
+Break & Continue
+"""
+playerhp = 260
+enemydmg = 60
+
+while playerhp > 0:
+    playerhp -= enemydmg
+
+    if playerhp <= 30
+        playerhp = 30
+    
+    print("Enemy attacked player... Current Health:", playerhp)
+
+    # As long as this statement is true
+    if playerhp > 30:
+        # It'll continue the loop and ignore eveything below it
+        continue
+    
+    print("Health is low, flee successful")
+    # since players health never goes below 0, we need to break out of the loop
+    break
