dcf3d546823fc725ce08ef80157fa77d46e4a418
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-14 23:20:45 +1100

    Filter through list to generate json object of meetup event content

--- webpy-meetup/Controller.py
+++ webpy-meetup/Controller.py
@@ -2,6 +2,7 @@ import web
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
 
 web.config.debug = False
 
@@ -22,13 +23,28 @@ class Home:
 class RetrievePost:
     def POST(self):
         data = web.input()
-        url = 'https://www.meetup.com/en-AU/find/' + data.topic + '/'
+        url = 'https://www.meetup.com/en-AU/find/' + data.topic.lower() + '/'
         r = requests.get(url)
         soup = BeautifulSoup(r.text, "html.parser")
         ul = soup.find("div", {"id": "simple-view"})
         ul = ul.find("ul", {"class": "j-groupCard-list"})
-        print(ul)
-        return "success"
+        data = []
+        list_items = ul.findAll('li', {'class': 'groupCard'})
+
+        for item in list_items:
+            link = item.find('a', {'class': 'groupCard--photo'}).attrs['href']
+            title = item.find('h3').text
+            image_url = item.find('a', {'class': 'groupCard--photo'}).get('style')
+            title = ' '.join(title.split())
+            obj = {
+                'link': link,
+                'title': re.sub('\t\n+', '', title),
+                'imageUrl': image_url
+            }
+            data.append(obj)
+
+        data = json.dumps(data)
+        return data
 
 
 if __name__ == "__main__":
