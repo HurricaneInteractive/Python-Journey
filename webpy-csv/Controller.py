56072e1ac03f824051fd7e7b245f33af2e893da7
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-16 21:49:14 +1100

    Temporary hashed file names

--- webpy-csv/Controller.py
+++ webpy-csv/Controller.py
@@ -1,8 +1,8 @@
 import web
 import csv
-import io
 import json
 import os
+from hashlib import blake2b
 
 urls = (
     '/', 'Home',
@@ -22,16 +22,16 @@ class ProcessCSV:
     def POST(self):
         file = web.input(csv_upload={})
         data_lines = []
-        with open('files/upload.csv', 'wb') as f:
+        file_hash = blake2b(b'Hacked').hexdigest()
+        with open('files/' + file_hash + '.csv', 'wb') as f:
             f.write(file['csv_upload'].value)
         
-        with open('files/upload.csv', 'r') as csv_file:
+        with open('files/' + file_hash + '.csv', 'r') as csv_file:
             csv_reader = csv.DictReader(csv_file)
-            header = next(csv_reader)
             for row in csv_reader:
                 data_lines.append(dict(row))
         
-        os.remove('files/upload.csv');
+        os.remove('files/' + file_hash + '.csv')
         return json.dumps(data_lines)
 
 
