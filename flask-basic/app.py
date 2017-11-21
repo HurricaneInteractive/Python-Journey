42ddf23ff91255f656fc7629361aa29922777d8d
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-21 23:15:08 +1100

    Included wtforms

--- flask-basic/app.py
+++ flask-basic/app.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template, request
+from wtforms import Form, FileField, StringField, validators
+import os
 
 app = Flask(__name__)
 app.debug = True
 
+class CSVForm(Form):
+    csvfile = FileField(u'CSV File')
+    delimiter = StringField(u'Delimiter (Default comma)')
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
+    form = CSVForm(request.form)
+    UPLOAD_PATH = 'uploads/'
+    if request.method == 'POST' and form.validate():
         return 'ok'
-    return render_template('home.html')
+    return render_template('home.html', form=form)
 
 if __name__ == '__main__':
     app.run()
