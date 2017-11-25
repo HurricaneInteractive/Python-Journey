cfb2ae6f35be7b83d5525fb44c42763355938263
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-25 17:18:46 +1100

    Created simple React Router config with 2 pages and a 404 page

--- flask-react/fullstack-template/static/js/index.jsx
+++ flask-react/fullstack-template/static/js/index.jsx
@@ -1,5 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('content'))
+import {
+    BrowserRouter as Router,
+    Route,
+    Link,
+    Switch
+} from 'react-router-dom'
+
+// Components
+import App from './App'
+import About from './About'
+import NoMatch from './NoMatch'
+
+ReactDOM.render(
+    <Router>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/no-match">No Match</Link></li>
+            </ul>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/about" component={About} />
+                <Route component={NoMatch} />
+            </Switch>
+        </div>    
+    </Router>, 
+    document.getElementById('content')
+)
