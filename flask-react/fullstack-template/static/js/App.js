df1cb42bda985f4ac2b193d9c8093ff067a5a43a
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-27 21:30:02 +1100

    Fetch and display retrieved items

--- flask-react/fullstack-template/static/js/App.js
+++ flask-react/fullstack-template/static/js/App.js
@@ -1,7 +1,40 @@
 import React from 'react'
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            items: null
+        }
+    }
+
+    componentDidMount() {
+        const _this = this
+        fetch('/items', {method: 'GET'})
+            .then((resp) => resp.json())
+            .then(function(data) {
+                  _this.setState({
+                      items: data.items
+                  })
+            })
+    }
+
     render() {
-        return <h1>Home Page</h1>
+        if (this.state.items == null) {
+            return <h1>Loading...</h1>
+        }
+        
+        return (
+            <div className="container">
+                <h2>Items from endpoint: <pre>/items</pre></h2>
+                <ul>
+                    {
+                        this.state.items.map((item, index) => {
+                            return <li key={index}>{item.name}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        )
     }
 }
