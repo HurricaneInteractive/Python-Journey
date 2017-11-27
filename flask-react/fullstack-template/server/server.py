b6651682f12308b2862ac550be47579b668d7b5c
Adriaan Janse van Rensburg <hurricane.interactive@gmail.com>
2017-11-27 21:29:42 +1100

    Added /items endpoint to api

--- flask-react/fullstack-template/server/server.py
+++ flask-react/fullstack-template/server/server.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask_restful import Resource, Api
 
 
 app = Flask(__name__, static_folder="../static/dist", template_folder="../static")
 app.debug = False
+api = Api(app)
 
 
 @app.route("/", defaults={'path': ''})
@@ -10,6 +12,21 @@ app.debug = False
 def index(path):
     return render_template('index.html')
 
+items = [
+    {
+        'name': 'Bread'
+    },
+    {
+        'name': 'Pasta'
+    }
+]
+
+class ItemList(Resource):
+    def get(self):
+        return {'items': items}
+
+
+api.add_resource(ItemList, '/items')
 
 if __name__ == "__main__":
     app.run()
